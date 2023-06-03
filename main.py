e62551bfca84c5e604edd143a43d1e2390e2516b
Devansh-Kesan <132878552+Devansh-Kesan@users.noreply.github.com>
2023-06-03 18:21:09 +0530

    Updated some functions in main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-
 from kivy.config import Config
 
 Config.set('graphics', 'width', '900')
@@ -10,8 +9,9 @@ from kivy.core.window import Window
 from kivy.app import App
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line, Quad, Triangle , Ellipse
-from kivy.properties import NumericProperty, Clock, ObjectProperty, StringProperty
+from kivy.properties import NumericProperty, Clock, ObjectProperty, StringProperty , ListProperty
 from kivy.uix.widget import Widget
+from kivy.uix.image import Widget
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.lang.builder import Builder
 from kivy.core.audio import SoundLoader
@@ -21,55 +21,57 @@ Builder.load_file("options.kv")
 
 
 class MainWidget(RelativeLayout):
-    from transforms import transform, transform_2D, transform_perspective
-    from user_actions import keyboard_closed, on_keyboard_up, on_keyboard_down, on_touch_up, on_touch_down
+    from transforms import transforming_function, transforming_function_for_2D, transforming_function_for_perspective
+    from user_actions import keyboard_is_closed, on_press_keyboard_up, on_press_keyboard_down, on_touching_up, on_touching_down
 
-    menu_widget = ObjectProperty()
+    #menu_widget = ObjectProperty()
     options_widget = ObjectProperty()
-    perspective_point_x = NumericProperty(0)
-    perspective_point_y = NumericProperty(0)
+    point_x_in_perspective_view = NumericProperty(0)
+    point_y_in_perspective_view = NumericProperty(0)
+    #list_color = ListProperty(random.choice([[0,0,0,1],[1,1,1,1]]))
 
-    V_NB_LINES = 18
-    V_LINES_SPACING = .4  # percentage in screen width
-    vertical_lines = []
+    Num_vertical_lines = 18
+    Space_vertical_lines = .4  # percentage in screen width
+    List_of_VLines = []
 
-    H_NB_LINES = 25
-    H_LINES_SPACING = .1  # percentage in screen height
-    horizontal_lines = []
+    Num_horizontal_lines = 25
+    Space_horizontal_lines = .1  # percentage in screen height
+    List_of_HLines = []
 
-    SPEED = 0
-    current_offset_y = 0
-    current_yloop = 0
+    speed_of_track = 0
+    present_offest_y = 0
+    present_yloop = 0
     # score=0
     speed_increment = 0.1/60
     speed_increment_per_score = 20
+    #color_change_with_score = 5
 
-    SPEED_X = 0
-    current_speed_x = 0
-    current_offset_x = 0
+    speed_X = 0
+    present_speed_x = 0
+    present_offset_x = 0
 
-    NB_TILES = 16
-    tiles = []
-    tiles_coordinates = []
+    len_visible_tiles = 16
+    Tiles = []
+    Tiles_coordinates = []
 
-    SHIP_WIDTH = .08
-    SHIP_HEIGHT = 0.035
-    SHIP_BASE_Y = 0.04
-    ship = None
-    ship_coordinates = [(0, 0), (0, 0), (0, 0)]
+    rocket_WIDTH = .1
+    rocket_HEIGHT = 0.035
+    rocket_BASE_Y = 0.04
+    rocket = None
+    rocket_coordinates = [(0, 0), (0, 0), (0, 0)]
 
-    state_game_over = False
+    Game_Over_Boolean = False
     state_game_has_started = False
 
     #menu_title = StringProperty("N I G H T M A R E")
-    options_title = StringProperty("SELECT AT YOUR OWN RISK")
+    options_title = StringProperty("M I S S I O N\n    M O O N ")
     #
     # menu_button_title = StringProperty("START")
     options_button_title_easy = StringProperty("Easy")
     options_button_title_medium = StringProperty("Medium")
     options_button_title_hard = StringProperty("Hard")
-    score_txt = StringProperty()
-    best_score_txt = StringProperty()
+    score_counter = StringProperty()
+    best_score_counter = StringProperty()
 
     score_bag = [0]
 
@@ -83,27 +85,27 @@ class MainWidget(RelativeLayout):
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         # print("INIT W:" + str(self.width) + " H:" + str(self.height))
-        self.init_vertical_lines()
-        self.init_horizontal_lines()
-        self.init_tiles()
-        self.init_ship()
-        self.reset_game()
-        self.init_audio()
+        self.init_Vertical_Lines()
+        self.init_Horizontal_Lines()
+        self.init_Tiles()
+        self.init_rocket()
+        self.Restart_Game()
+        self.init_Audio_File()
 
-        self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
-        self._keyboard.bind(on_key_down=self.on_keyboard_down)
-        self._keyboard.bind(on_key_up=self.on_keyboard_up)
+        self._keyboard = Window.request_keyboard(self.keyboard_is_closed, self)
+        self._keyboard.bind(on_key_down=self.on_press_keyboard_down)
+        self._keyboard.bind(on_key_up=self.on_press_keyboard_up)
 
-        Clock.schedule_interval(self.update, 1.0 / 60.0)
+        Clock.schedule_interval(self.Update, 1.0 / 60.0)
         self.sound_galaxy.play()
     
 
-    def init_audio(self):
+    def init_Audio_File(self):
         self.sound_begin = SoundLoader.load("audio/begin.wav")
         self.sound_galaxy = SoundLoader.load("audio/galaxy.wav")
         self.sound_gameover_impact = SoundLoader.load("audio/gameover_impact.wav")
         self.sound_gameover_voice = SoundLoader.load("audio/gameover_voice.wav")
-        self.sound_music1 = SoundLoader.load("audio/music1.wav")
+        self.sound_music1 = SoundLoader.load("fight-142564.mp3")
         self.sound_restart = SoundLoader.load("audio/restart.wav")
 
         self.sound_music1.volume = 1
@@ -114,74 +116,80 @@ class MainWidget(RelativeLayout):
         self.sound_gameover_impact.volume = .6
     
     
-    def reset_game(self):
+    def Restart_Game(self):
 
-        self.tiles_coordinates = []
-        self.pre_fill_tiles_coordinates()
-        self.generate_tiles_coordinates()
-        self.current_offset_y = 0
-        self.current_yloop = 0
-        self.current_speed_x = 0
-        self.current_offset_x = 0
-        self.score_txt = "SCORE: " + str(self.current_yloop)
-        self.best_score_txt = "BEST SCORE: " + str(max(self.score_bag))
+        self.Tiles_coordinates = []
+        self.starting_tiles_coordinates()
+        self.produce_tiles_coordinates()
+        self.present_offest_y = 0
+        self.present_yloop = 0
+        self.present_speed_x = 0
+        self.present_offset_x = 0
+        self.score_counter = "SCORE: " + str(self.present_yloop)
+        self.best_score_counter = "BEST SCORE: " + str(max(self.score_bag))
 
 
 
-        self.state_game_over = False
+        self.Game_Over_Boolean = False
 
-    def is_desktop(self):
+    def Is_Desktop(self):
         if platform in ('linux', 'windows', 'macosx'):
             return True
         return False
 
-    def init_ship(self):
+    
+    def init_rocket(self):
+        color = (random.random(),random.random(), random.random())
         with self.canvas:
-            Color(0,0,1)
-            self.ship = Triangle()
+            
+            Color(*color)
+            self.rocket = Triangle()
 
-    def update_ship(self):
+    def Update_rocket(self):
         center_x = self.width / 2
-        base_y = self.SHIP_BASE_Y * self.height
-        ship_half_width = self.SHIP_WIDTH * self.width / 2
-        ship_height = self.SHIP_HEIGHT * self.height
+        base_y = self.rocket_BASE_Y * self.height
+        rocket_half_width = self.rocket_WIDTH * self.width / 2
+        rocket_height = self.rocket_HEIGHT * self.height
         # ....
         #    2
         #  1   3
-        # self.transform
-        self.ship_coordinates[0] = (center_x-ship_half_width, base_y)
-        self.ship_coordinates[1] = (center_x, base_y + ship_height)
-        self.ship_coordinates[2] = (center_x + ship_half_width, base_y)
+        # self.transforming_function
+        self.rocket_coordinates[0] = (center_x-rocket_half_width, base_y)
+        self.rocket_coordinates[1] = (center_x, base_y + rocket_height)
+        self.rocket_coordinates[2] = (center_x + rocket_half_width, base_y)
 
-        x1, y1 = self.transform(*self.ship_coordinates[0])
-        x2, y2 = self.transform(*self.ship_coordinates[1])
-        x3, y3 = self.transform(*self.ship_coordinates[2])
+        a1, b1 = self.transforming_function(*self.rocket_coordinates[0])
+        a2, b2 = self.transforming_function(*self.rocket_coordinates[1])
+        a3, b3 = self.transforming_function(*self.rocket_coordinates[2])
 
-        self.ship.points = [x1, y1, x2, y2, x3, y3]
+        self.rocket.points = [a1, b1, a2, b2, a3, b3]
 
-    def check_ship_collision(self):
-        for i in range(0, len(self.tiles_coordinates)):
-            ti_x, ti_y = self.tiles_coordinates[i]
-            if ti_y > self.current_yloop + 1:
+    def check_rocket_collision(self):
+        for i in range(0, len(self.Tiles_coordinates)):
+            tile_x, tile_y = self.Tiles_coordinates[i]
+            if tile_y > self.present_yloop + 1:
                 return False
-            if self.check_ship_collision_with_tile(ti_x, ti_y):
+            if self.check_rocket_collision_with_tile(tile_x, tile_y):
                 return True
         return False
 
-    def check_ship_collision_with_tile(self, ti_x, ti_y):
-        xmin, ymin = self.get_tile_coordinates(ti_x, ti_y)
-        xmax, ymax = self.get_tile_coordinates(ti_x + 1, ti_y + 1)
+    def check_rocket_collision_with_tile(self, tile_x, tile_y):
+        x_min, y_min = self.obtain_tile_coordinates(tile_x, tile_y)
+        x_max, y_max = self.obtain_tile_coordinates(tile_x + 1, tile_y + 1)
         for i in range(0, 3):
-            px, py = self.ship_coordinates[i]
-            if xmin <= px <= xmax and ymin <= py <= ymax:
+            p_x, p_y = self.rocket_coordinates[i]
+            if x_min <= p_x <= x_max and y_min <= p_y <= y_max:
                 return True
         return False
+    
 
-    def init_tiles(self):
+
+    def init_Tiles(self):
+        #color = (random.random(),random.random(), random.random())
         with self.canvas:
-            Color(0, 0, 0,1)
-            for i in range(0, self.NB_TILES):
-                self.tiles.append(Quad())
+            Color(0,0,0,0.9)
+            for i in range(0, self.len_visible_tiles):
+                self.Tiles.append(Quad())
 
     def starting_tiles_coordinates(self):
         for i in range(0, 9):
