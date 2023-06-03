1a313975fccd312b10123fda59160febaa2c15ef
swap5113github <133361070+swap5113github@users.noreply.github.com>
2023-06-03 18:10:36 +0530

    Updated transforms.py

--- transforms.py
+++ transforms.py
@@ -1,21 +1,21 @@
-def transform(self,x,y):
-        # return self.transform_2D(x,y)
-        return self.transform_perspective(x,y)
+def transforming_function(self, a, b):
+    # return self.transforming_function_for_2D(a, b)
+    return self.transforming_function_for_perspective(a, b)
 
-def transform_2D(self,x,y):
-        return int(x),int(y)
+def transforming_function_for_2D(self, a, b):
+    return int(a), int(b)
 
-def transform_perspective(self,x,y):
-    lin_y= y * self.perspective_point_y / self.height
-    if lin_y>self.perspective_point_y:
-        lin_y=self.perspective_point_y
-    
-    diff_x=x-self.perspective_point_x
-    diff_y=self.perspective_point_y-lin_y
-    factor_y=diff_y/self.perspective_point_y
-    factor_y=pow(factor_y,4)
+def transforming_function_for_perspective(self, a, b):
+    lin_b = b * self.point_y_in_perspective_view / self.height
+    if lin_b > self.point_y_in_perspective_view:
+        lin_b = self.point_y_in_perspective_view
 
-    tr_x=self.perspective_point_x+diff_x*factor_y
-    tr_y=self.perspective_point_y - factor_y*self.perspective_point_y
-    
-    return int(tr_x),int(tr_y)
+    diff_a = a-self.point_x_in_perspective_view
+    diff_b = self.point_y_in_perspective_view-lin_b
+    factor_b = diff_b/self.point_y_in_perspective_view  # 1 when diff_b == self.point_y_in_perspective_view / 0 when diff_y = 0
+    factor_b = pow(factor_b, 4)
+
+    tr_a = self.point_x_in_perspective_view + diff_a*factor_b
+    tr_b = self.point_y_in_perspective_view - factor_b*self.point_y_in_perspective_view
+
+    return int(tr_a), int(tr_b)
