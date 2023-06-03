60cd34eab018a6fe0866d1708ca57b831737c028
Devansh-Kesan <132878552+Devansh-Kesan@users.noreply.github.com>
2023-06-03 18:19:24 +0530

    Updated users_actions.py

--- users_actions.py
+++ users_actions.py
@@ -1,30 +1,36 @@
+import random
 from kivy.uix.relativelayout import RelativeLayout
-
-def keyboard_closed(self):
-    self._keyboard.unbind(on_key_down=self.on_keyboard_down)
-    self._keyboard.unbind(on_key_up=self.on_keyboard_up)
+from kivy.graphics.vertex_instructions import Triangle
+from kivy.graphics import Color
+from kivy.properties import ObjectProperty
+# from transforms import transform, transform_2D, transform_perspective
+#triangle = ObjectProperty(None)
+def keyboard_is_closed(self):
+    self._keyboard.unbind(on_key_down=self.on_press_keyboard_down)
+    self._keyboard.unbind(on_key_up=self.on_press_keyboard_up)
     self._keyboard = None
 
-def on_keyboard_down(self, keyboard, keycode, text, modifiers):
+def on_press_keyboard_down(self, keyboard, keycode, text, modifiers):
     if keycode[1] == 'left':
-        self.current_speed_x = self.SPEED_X
+        self.present_speed_x = self.speed_X
     elif keycode[1] == 'right':
-        self.current_speed_x = -self.SPEED_X
+        self.present_speed_x = -self.speed_X
     return True
 
-def on_keyboard_up(self, keyboard, keycode):
-    self.current_speed_x = 0
+def on_press_keyboard_up(self, keyboard, keycode):
+    self.present_speed_x = 0
     return True
 
-def on_touch_down(self, touch):
+def on_touching_down(self, touch):
     if touch.x < self.width/2:
         # print("<-")
-        self.current_speed_x = self.SPEED_X
+        self.present_speed_x = self.speed_X
     else:
         # print("->")
-        self.current_speed_x = -self.SPEED_X
-    return super(RelativeLayout,self).on_touch_down(touch)
+        self.present_speed_x = -self.speed_X
+    return super(RelativeLayout,self).on_touching_down(touch)
+    
 
-def on_touch_up(self, touch):
+def on_touching_up(self, touch):
     #print("UP")
-    self.current_speed_x = 0
+    self.present_speed_x = 0
