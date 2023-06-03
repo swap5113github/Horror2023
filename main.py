597f542c19d7e20b9aebb7cda5febf2347fc5971
swap5113github <133361070+swap5113github@users.noreply.github.com>
2023-06-03 18:18:03 +0530

    Updated some functions in main.py

--- main.py
+++ main.py
@@ -183,141 +183,156 @@ class MainWidget(RelativeLayout):
             for i in range(0, self.NB_TILES):
                 self.tiles.append(Quad())
 
-    def pre_fill_tiles_coordinates(self):
-        for i in range(0, 10):
-            self.tiles_coordinates.append((0, i))
+    def starting_tiles_coordinates(self):
+        for i in range(0, 9):
+            self.Tiles_coordinates.append((0, i))
 
-    def generate_tiles_coordinates(self):
-        last_x = 0
-        last_y = 0
-        for i in range(len(self.tiles_coordinates), self.NB_TILES):
+    def produce_tiles_coordinates(self):
+        LAST_x = 0
+        LAST_y = 0
+
+        # clean the coordinates that are out of the screen
+        # tile_y < self.present_yloop
+        for i in range(len(self.Tiles_coordinates)-1, -1, -1):
+            if self.Tiles_coordinates[i][1] < self.present_yloop:
+                del self.Tiles_coordinates[i]
+
+        if len(self.Tiles_coordinates) > 0:
+            LAST_coordinates = self.Tiles_coordinates[-1]
+            LAST_x = LAST_coordinates[0]
+            LAST_y = LAST_coordinates[1] + 1
+
+        #print("foo1")
+
+        for i in range(len(self.Tiles_coordinates), self.len_visible_tiles):
             r = random.randint(0, 2)
             # 0 -> straight
             # 1 -> right
             # 2 -> left
-            start_index = -int(self.V_NB_LINES / 2) + 1
-            end_index = start_index + self.V_NB_LINES - 1
-            if last_x <= start_index:
+            starting_index = -int(self.Num_vertical_lines / 2) + 1
+            ending_index = starting_index + self.Num_vertical_lines - 1
+            if LAST_x <= starting_index:
                 r = 1
-            if last_x >= end_index:
+            if LAST_x >= ending_index:
                 r = 2
 
-            self.tiles_coordinates.append((last_x, last_y))
+            self.Tiles_coordinates.append((LAST_x, LAST_y))
             if r == 1:
-                last_x += 1
-                self.tiles_coordinates.append((last_x, last_y))
-                last_y += 1
-                self.tiles_coordinates.append((last_x, last_y))
+                LAST_x += 1
+                self.Tiles_coordinates.append((LAST_x, LAST_y))
+                LAST_y += 1
+                self.Tiles_coordinates.append((LAST_x, LAST_y))
             if r == 2:
-                last_x -= 1
-                self.tiles_coordinates.append((last_x, last_y))
-                last_y += 1
-                self.tiles_coordinates.append((last_x, last_y))
+                LAST_x -= 1
+                self.Tiles_coordinates.append((LAST_x, LAST_y))
+                LAST_y += 1
+                self.Tiles_coordinates.append((LAST_x, LAST_y))
 
-            last_y += 1
+            LAST_y += 1
 
         #print("foo2")
 
-    def init_vertical_lines(self):
+    def init_Vertical_Lines(self):
         with self.canvas:
             Color(0, 0, 0,0)
             #self.line = Line(points=[100, 0, 100, 100])
-            for i in range(0, self.V_NB_LINES):
-                self.vertical_lines.append(Line())
+            for i in range(0, self.Num_vertical_lines):
+                self.List_of_VLines.append(Line())
 
-    def get_line_x_from_index(self, index):
-        central_line_x = self.perspective_point_x
-        spacing = self.V_LINES_SPACING * self.width
-        offset = index - 0.5
-        line_x = central_line_x + offset*spacing + self.current_offset_x
-        return line_x
+    def from_index_obtain_line_x(self, index):
+        mid_line_x = self.point_x_in_perspective_view
+        screen_dependent_spacing = self.Space_vertical_lines * self.width
+        ofset = index - 0.5
+        single_line_x = mid_line_x + ofset*screen_dependent_spacing + self.present_offset_x
+        return single_line_x
 
-    def get_line_y_from_index(self, index):
-        spacing_y = self.H_LINES_SPACING*self.height
-        line_y = index*spacing_y-self.current_offset_y
-        return line_y
+    def from_index_obtain_line_y(self, index):
+        screen_dependent_spacing_y = self.Space_horizontal_lines*self.height
+        single_line_y = index*screen_dependent_spacing_y-self.present_offest_y
+        return single_line_y
 
-    def get_tile_coordinates(self, ti_x, ti_y):
-        ti_y = ti_y - self.current_yloop
-        x = self.get_line_x_from_index(ti_x)
-        y = self.get_line_y_from_index(ti_y)
+    def obtain_tile_coordinates(self, tile_x, tile_y):
+        tile_y = tile_y - self.present_yloop
+        x = self.from_index_obtain_line_x(tile_x)
+        y = self.from_index_obtain_line_y(tile_y)
         return x, y
 
-    def update_tiles(self):
-        for i in range(0, self.NB_TILES):
-            tile = self.tiles[i]
-            tile_coordinates = self.tiles_coordinates[i]
-            xmin, ymin = self.get_tile_coordinates(tile_coordinates[0], tile_coordinates[1])
-            xmax, ymax = self.get_tile_coordinates(tile_coordinates[0]+1, tile_coordinates[1]+1)
+    def Update_tiles(self):
+        # self.Update_path_color()
+        for i in range(0, self.len_visible_tiles):
+            specific_tile = self.Tiles[i]
+            specific_tile_coordinates = self.Tiles_coordinates[i]
+            x_min, y_min = self.obtain_tile_coordinates(specific_tile_coordinates[0], specific_tile_coordinates[1])
+            x_max, y_max = self.obtain_tile_coordinates(specific_tile_coordinates[0]+1, specific_tile_coordinates[1]+1)
 
             #  2    3
             #
             #  1    4
-            x1, y1 = self.transform(xmin, ymin)
-            x2, y2 = self.transform(xmin, ymax)
-            x3, y3 = self.transform(xmax, ymax)
-            x4, y4 = self.transform(xmax, ymin)
+            a1, b1 = self.transforming_function(x_min, y_min)
+            a2, b2 = self.transforming_function(x_min, y_max)
+            a3, b3 = self.transforming_function(x_max, y_max)
+            a4, b4 = self.transforming_function(x_max, y_min)
 
-            tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
+            specific_tile.points = [a1, b1, a2, b2, a3, b3, a4, b4]
 
-    def update_vertical_lines(self):
+    def Update_vertical_lines(self):
         # -1 0 1 2
-        start_index = -int(self.V_NB_LINES/2)+1
-        for i in range(start_index, start_index+self.V_NB_LINES):
-            line_x = self.get_line_x_from_index(i)
+        starting_index = -int(self.Num_vertical_lines/2)+1
+        for i in range(starting_index, starting_index+self.Num_vertical_lines):
+            single_line_x = self.from_index_obtain_line_x(i)
 
-            x1, y1 = self.transform(line_x, 0)
-            x2, y2 = self.transform(line_x, self.height)
-            self.vertical_lines[i].points = [x1, y1, x2, y2]
+            a1, b1 = self.transforming_function(single_line_x, 0)
+            a2, b2 = self.transforming_function(single_line_x, self.height)
+            self.List_of_VLines[i].points = [a1, b1, a2, b2]
 
-    def init_horizontal_lines(self):
+    def init_Horizontal_Lines(self):
         with self.canvas:
             Color(0, 0, 0,0)
-            for i in range(0, self.H_NB_LINES):
-                self.horizontal_lines.append(Line())
+            for i in range(0, self.Num_horizontal_lines):
+                self.List_of_HLines.append(Line())
 
-    def update_horizontal_lines(self):
-        start_index = -int(self.V_NB_LINES / 2) + 1
-        end_index = start_index+self.V_NB_LINES-1
+    def Update_horizontal_lines(self):
+        starting_index = -int(self.Num_vertical_lines / 2) + 1
+        ending_index = starting_index+self.Num_vertical_lines-1
 
-        xmin = self.get_line_x_from_index(start_index)
-        xmax = self.get_line_x_from_index(end_index)
-        for i in range(0, self.H_NB_LINES):
-            line_y = self.get_line_y_from_index(i)
-            x1, y1 = self.transform(xmin, line_y)
-            x2, y2 = self.transform(xmax, line_y)
-            self.horizontal_lines[i].points = [x1, y1, x2, y2]
+        x_min = self.from_index_obtain_line_x(starting_index)
+        x_max = self.from_index_obtain_line_x(ending_index)
+        for i in range(0, self.Num_horizontal_lines):
+            single_line_y = self.from_index_obtain_line_y(i)
+            a1, b1 = self.transforming_function(x_min, single_line_y)
+            a2, b2 = self.transforming_function(x_max, single_line_y)
+            self.List_of_HLines[i].points = [a1, b1, a2, b2]
 
-    def update(self, dt):
+    def Update(self, dt):
         # print("dt: " + str(dt*60))
         time_factor = dt*60
-        self.update_vertical_lines()
-        self.update_horizontal_lines()
-        self.update_tiles()
-        self.update_ship()
-        self.update_score_bag()
-        # self.score=self.current_yloop
-        # print(self.current_yloop)
-        self.update_speed_per_score()
+        self.Update_vertical_lines()
+        self.Update_horizontal_lines()
+        self.Update_tiles()
+        self.Update_rocket()
+        self.Update_score_bag()
+        # self.score=self.present_yloop
+        # print(self.present_yloop)
+        self.Update_speed_per_score()
 
-        if not self.state_game_over and self.state_game_has_started:
+        if not self.Game_Over_Boolean and self.state_game_has_started:
             
-            speed_y = self.SPEED * self.height / 100
-            self.current_offset_y += speed_y * time_factor
+            screen_dependent_y_speed = self.speed_of_track * self.height / 100
+            self.present_offest_y += screen_dependent_y_speed * time_factor
 
-            spacing_y = self.H_LINES_SPACING * self.height
-            if self.current_offset_y >= spacing_y:
-                self.current_offset_y -= spacing_y
-                self.current_yloop += 1
-                self.score_txt = "SCORE: " + str(self.current_yloop)
-                self.generate_tiles_coordinates()
-                #print("loop : " + str(self.current_yloop))
+            screen_dependent_spacing_y = self.Space_horizontal_lines * self.height
+            if self.present_offest_y >= screen_dependent_spacing_y:
+                self.present_offest_y -= screen_dependent_spacing_y
+                self.present_yloop += 1
+                self.score_counter = "SCORE: " + str(self.present_yloop)
+                self.produce_tiles_coordinates()
+                #print("loop : " + str(self.present_yloop))
 
-            speed_x = self.current_speed_x * self.width / 100
-            self.current_offset_x += speed_x * time_factor
+            screen_dependent_x_speed = self.present_speed_x * self.width / 100
+            self.present_offset_x += screen_dependent_x_speed * time_factor
 
-        if not self.check_ship_collision() and not self.state_game_over:
-            self.state_game_over = True
+        if not self.check_rocket_collision() and not self.Game_Over_Boolean:
+            self.Game_Over_Boolean = True
             self.options_title = "G A M E  O V E R\n\n   R E S T A R T"
             self.options_button_title_easy = "EASY"
             self.options_button_title_medium = "MEDIUM"
@@ -326,69 +341,70 @@ class MainWidget(RelativeLayout):
             self.sound_music1.stop()
             self.sound_gameover_impact.play()
             Clock.schedule_once(self.play_game_over_voice_sound, 3)
-    def update_score_bag(self):
-        if self.state_game_over:
-            self.score_bag.append(self.current_yloop)
+    def Update_score_bag(self):
+        if self.Game_Over_Boolean:
+            self.score_bag.append(self.present_yloop)
     
-    def update_speed_per_score(self):
-        if self.current_yloop % self.speed_increment_per_score == 0 and self.current_yloop!=0 :
-            self.SPEED += self.speed_increment
+    def Update_speed_per_score(self):
+        if self.present_yloop % self.speed_increment_per_score == 0 and self.present_yloop!=0 :
+            self.speed_of_track += self.speed_increment
             
     def play_game_over_voice_sound(self, dt):
-        if self.state_game_over:
+        if self.Game_Over_Boolean:
             self.sound_gameover_voice.play()
     
     #def on_menu_button_pressed(self):
 
 
     def on_options_button_easy_pressed(self):
-        if self.state_game_over:
+        if self.Game_Over_Boolean:
             self.sound_restart.play()
             
         else:
             self.sound_begin.play()
         self.sound_music1.play()
         self.state_game_has_started = True
-        self.SPEED = 0.4
-        print(self.current_yloop)
-        self.update_speed_per_score()
-        self.SPEED_X = 2.0
-        print(self.SPEED)
+        self.speed_of_track = 0.4
+        #print(self.present_yloop)
+        self.Update_speed_per_score()
+        self.speed_X = 2.0
+        #print(self.speed_of_track)
         self.options_widget.opacity = 0
-        self.reset_game()
+        self.Restart_Game()
         
         
     def on_options_button_medium_pressed(self):
-        if self.state_game_over:
+        if self.Game_Over_Boolean:
             self.sound_restart.play()
             
         else:
             self.sound_begin.play()
         self.sound_music1.play()
         self.state_game_has_started = True
-        self.SPEED = 0.6
-        self.update_speed_per_score()
-        self.SPEED_X = 2.0
+        self.speed_of_track = 0.6
+        self.Update_speed_per_score()
+        self.speed_X = 2.0
         self.options_widget.opacity = 0
-        self.reset_game()
+        self.Restart_Game()
     def on_options_button_hard_pressed(self):
-        if self.state_game_over:
+        if self.Game_Over_Boolean:
             self.sound_restart.play()
             
         else:
             self.sound_begin.play()
+        self.sound_music1.loop
         self.sound_music1.play()
         self.state_game_has_started = True
-        self.SPEED = 0.9
-        self.update_speed_per_score()
-        self.SPEED_X = 2.0
+        self.speed_of_track = 0.9
+        self.Update_speed_per_score()
+        self.speed_X = 2.0
         self.options_widget.opacity = 0
-        self.reset_game()
+        self.Restart_Game()
 
 
-class GalaxyApp(App):
+class Mission_MoonApp(App):
     pass
 
 
-GalaxyApp().run()
+Mission_MoonApp().run()
      
