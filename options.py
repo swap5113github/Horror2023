2071a66578d7de5092d292af9f4763c2be8bd93d
Devansh-Kesan <132878552+Devansh-Kesan@users.noreply.github.com>
2023-06-09 22:29:50 +0530

    Add files via upload

---
+++ options.py
@@ -0,0 +1,8 @@
+from kivy.uix.relativelayout import RelativeLayout
+
+class OptionsWidget(RelativeLayout):
+    def on_touch_down(self, touch):
+        if self.opacity==0:
+            return False
+
+        return super(RelativeLayout,self).on_touch_down(touch)
